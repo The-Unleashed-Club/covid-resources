6d324148cd207d1d129e8c68c92eb555f6f8cfd2
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-05-03 00:18:54 -0700

    headerComponent
    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+const Header = () => {
+    return (
+        <div style={styles.box}>
+            <h1 style={styles.head}>Recovered India project</h1>,
+        </div>
+    )
+};
+
+const styles ={
+    box: {
+        boxSizing: 'border-box',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'baseline',
+        background: '#FFFF00',
+    },
+    head:{
+        color:'#808080'
+    }
+
+};
+
+export default Header
