b5c9c5aa8e56edfd494afda3b128c9b4e33582bc
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-05-05 02:38:35 -0700

    stateDatabase
    state info

---
+++ src/components/StateData.jsx
@@ -0,0 +1,120 @@
+const StateData = [
+    {
+        img: '',
+        stateName: 'Andhra Pradesh'
+    },
+    {
+        img: '',
+        stateName: 'Assam'
+    },
+    {
+        img: '',
+        stateName: 'Arunachal Pradesh'
+    },
+    {
+        img: '',
+        stateName: 'Bihar'
+    },
+    {
+        img: '',
+        stateName: 'Chhattisgarh'
+    },
+    {
+        img: '',
+        stateName: 'Goa'
+    },
+    {
+        img: '',
+        stateName: 'Gujrat'
+    },
+    {
+        img: '',
+        stateName: 'Haryana'
+    },
+    {
+        img: '',
+        stateName: 'Himachal Pradesh'
+    },
+
+    {
+        img: '',
+        stateName: 'Himachal Pradesh'
+    },
+    {
+        img: '',
+        stateName: 'Jammu and Kashmir'
+    },
+    {
+        img: '',
+        stateName: 'Jharkhand'
+    },
+
+    {
+        img: '',
+        stateName: 'Karnataka'
+    },
+    {
+        img: '',
+        stateName: 'Kerala'
+    },
+    {
+        img: '',
+        stateName: 'Madhya Pradesh'
+    },
+    {
+        img: '',
+        stateName: 'Maharashtra'
+    },
+    {
+        img: '',
+        stateName: 'Manipur'
+    },
+    {
+        img: '',
+        stateName: 'Mizoram'
+    },
+    {
+        img: '',
+        stateName: 'Nagaland'
+    },
+    {
+        img: '',
+        stateName: 'Orissa'
+    },
+    {
+        img: '',
+        stateName: 'Punjab'
+    },
+    {
+        img: '',
+        stateName: 'Rajasthan'
+    },
+    {
+        img: '',
+        stateName: 'Sikkim'
+    },
+    {
+        img: '',
+        stateName: 'Tamil Nadu'
+    },
+    {
+        img: '',
+        stateName: 'Tripura'
+    },
+    {
+        img: '',
+        stateName: 'Uttaranchal'
+    },
+    {
+        img: '',
+        stateName: 'Uttar Pradesh'
+    },
+    {
+        img: '',
+        stateName: 'West Bengal'
+    },
+
+
+];
+
+export default StateData
