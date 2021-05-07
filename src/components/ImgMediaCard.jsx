02410924d2491c1130e008c5436a618f117c9cd2
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2021-05-07 19:44:19 +0530

    UpdatedCard
    Popup added

--- src/components/ImgMediaCard.jsx
+++ src/components/ImgMediaCard.jsx
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import SimplePopper from './PopUp';
 //import StateData from './StateData'
 
 const useStyles = makeStyles({
@@ -40,9 +41,7 @@ export default function ImgMediaCard(props) {
         <Button size="small" color="primary">
           Share
         </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
+        <SimplePopper />
       </CardActions>
     </Card>
   );
