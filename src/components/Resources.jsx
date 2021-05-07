a072bed2b4482bbb21b044aba009862915b4ded2
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2021-05-07 16:56:16 +0530

    Resources
    component for Resources

---
+++ src/components/Resources.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const Res = () => {
+  return (
+    <div>
+   <button>Oxygen Cylinders</button>
+   <button>Oxygen Refilling</button>
+   <button>Beds With Oxygen</button>
+   <button>Medicines</button>
+   <button>Ambulance</button>
+   <button>NGO</button>
+   
+
+</div>
+    
+  )
+};
+
+
+export default Res
