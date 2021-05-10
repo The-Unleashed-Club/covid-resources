04d7fa40d17be6ef0ceaadda1cf9a6aabe0f3f02
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-05-10 09:19:36 -0700

    DummyDataa
    andraPradesh dummy data

---
+++ src/StatesLeads/AndraPradesh.jsx
@@ -0,0 +1,56 @@
+const AndraPradeshData =[
+    {
+        city:"",
+        nameOfFacility:"",
+        address: "near Subhash Nagar mall",
+        contact: "8766357982",
+        avalability: "Oxygen cylinders Pre-booking and payment must be done today itself",
+        price:"50k Advance payment Pick up yourself.",
+        comments:"May reach teh place directly for refilling, charges are rs 200 for 10 kg",
+        VerficationDataTime :"",
+    },
+    {
+        city:"",
+        nameOfFacility:"",
+        address: "near Subhash Nagar mall",
+        contact: "8766357982",
+        avalability: "Oxygen cylinders Pre-booking and payment must be done today itself",
+        price:"50k Advance payment Pick up yourself.",
+        comments:"May reach teh place directly for refilling, charges are rs 200 for 10 kg",
+        VerficationDataTime :"",
+    },
+    {
+        city:"",
+        nameOfFacility:"",
+        address: "near Subhash Nagar mall",
+        contact: "8766357982",
+        avalability: "Oxygen cylinders Pre-booking and payment must be done today itself",
+        price:"50k Advance payment Pick up yourself.",
+        comments:"May reach teh place directly for refilling, charges are rs 200 for 10 kg",
+        VerficationDataTime :"",
+    },
+    {
+        city:"",
+        nameOfFacility:"",
+        address: "near Subhash Nagar mall",
+        contact: "8766357982",
+        avalability: "Oxygen cylinders Pre-booking and payment must be done today itself",
+        price:"50k Advance payment Pick up yourself.",
+        comments:"May reach teh place directly for refilling, charges are rs 200 for 10 kg",
+        VerficationDataTime :"",
+    },
+    {
+        city:"",
+        nameOfFacility:"",
+        address: "near Subhash Nagar mall",
+        contact: "8766357982",
+        avalability: "Oxygen cylinders Pre-booking and payment must be done today itself",
+        price:"50k Advance payment Pick up yourself.",
+        comments:"May reach teh place directly for refilling, charges are rs 200 for 10 kg",
+        VerficationDataTime :"",
+    },
+
+];
+
+
+export default AndraPradeshData
