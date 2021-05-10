f282403308d4fab5cd7b00fd5bd1d8b562e3f435
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-05-10 09:21:46 -0700

    stateLeads
    state leads (andrapradesh)

---
+++ src/components/StateLeads.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+//import ImgMediaCard from './ImgMediaCard';
+//import StateData from './StateData';
+import AndraPradeshData from '../StatesLeads/AndraPradesh';
+import OutlinedCard from './OutlinedCard'
+
+function CardData(val) {
+    return (
+        <OutlinedCard city={val.city}
+            nameOfFacility={val.nameOfFacility}
+            address={val.address}
+            contact={val.contact}
+            avalability={val.avalability}
+            price={val.price}
+            comments={val.comments}
+            VerficationDataTime={val.VerficationDataTime}
+        />
+    );
+}
+console.log();
+
+
+const States = (props) => {
+    return (
+        <div style={styles.items}>
+            {AndraPradeshData.map(CardData)}
+        </div>
+    )
+}
+
+const styles = {
+    items: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+
+};
+
+export default States
