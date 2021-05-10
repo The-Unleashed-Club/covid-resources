5671f8012212fb6d431cab335a3dc14b068a6e20
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-05-10 09:22:38 -0700

    screen second
    made screen second without navigation

---
+++ src/screens/Screen2.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+//import LeadCardData from '../components/LeadCardData';
+import StateLeads from '../components/StateLeads'
+
+const Screen2 = () => {
+    return (
+      <>
+        <Header />
+        <StateLeads/>
+        <Footer/>
+      </>
+    )
+  };
+  
+  
+  export default Screen2
