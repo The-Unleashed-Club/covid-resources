591e3f08d59502471e521e6bbe2a0e01d85c9318
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2021-05-13 02:49:21 +0530

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,74 +1,41 @@
 import React, { useState } from "react";
-
-//import react pro sidebar components
-import {
-  ProSidebar,
-  Menu,
-  MenuItem,
-  SidebarHeader,
-  //SidebarFooter,
-  SidebarContent,
-} from "react-pro-sidebar";
-
-// FiLogOut remove --k
-//import icons from react icons
-import { FaList, FaRegHeart } from "react-icons/fa";
-import { FiHome, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
-import { RiPencilLine } from "react-icons/ri";
-import { BiCog } from "react-icons/bi";
-
-
-//import sidebar css from react-pro-sidebar module and our custom css 
-import "react-pro-sidebar/dist/css/styles.css";
-import "./Nav.css";
-
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-  
-    //create initial menuCollapse state using useState hook
-    const [menuCollapse, setMenuCollapse] = useState(false)
-
-    //create a custom function that will change menucollapse state from false to true and true to false
-  const menuIconClick = () => {
-    //condition checking to change state from true to false and vice versa
-    menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
-  };
-
   return (
     <>
-      <div id="header">
-          {/* collapsed props to change menu size using menucollapse state */}
-        <ProSidebar collapsed={menuCollapse}>
-          <SidebarHeader>
-          <div className="logotext">
-              {/* small and big change using menucollapse state */}
-              <p>{menuCollapse ? "Logo" : "Big Logo"}</p>
-            </div>
-            <div className="closemenu" onClick={menuIconClick}>
-                {/* changing menu collapse icon on click */}
-              {menuCollapse ? (
-                <FiArrowRightCircle/>
-              ) : (
-                <FiArrowLeftCircle/>
-              )}
-            </div>
-          </SidebarHeader>
-          <SidebarContent>
-            <Menu iconShape="square">
-              <MenuItem active={true} icon={<FiHome />}>
-                Home
-              </MenuItem>
-              <MenuItem icon={<FaList />}>Category</MenuItem>
-              <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
-              <MenuItem icon={<RiPencilLine />}>Author</MenuItem>
-              <MenuItem icon={<BiCog />}>Settings</MenuItem>
-            </Menu>
-          </SidebarContent>
-          
-        </ProSidebar>
+      <div style={styles.Nav}>
+        <Link style={styles.btn} to="/About">About Us</Link>
+        <Link style={styles.btn} to="/Blog">Blog</Link>
+        <Link style={styles.btn} to="/Donate">Donate</Link>
+        <Link style={styles.btn} to="/Leads">Submit Verified Leads</Link>
       </div>
     </>
-  );
+  )
 };
 
-export default Nav;
+const styles = {
+  Nav: {
+    position: 'sticky',
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: '#808080',
+
+  },
+  btn: {
+    backgroundColor: '#FFE4E1',
+    color: '#000000',
+    paddingLeft: '0.5%',
+    paddingRight: '0.5%',
+    paddingTop: '0.2%',
+    paddingBottom: '0.2%',
+    margin: '1%',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderRadius: '5px',
+    textDecoration: 'none',
+
+  },
+};
+export default Nav
