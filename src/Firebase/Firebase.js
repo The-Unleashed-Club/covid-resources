81b29db3bb17b76f795eee728c314c6224d2a413
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-05-27 15:31:38 -0700

    update

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,5 +1,10 @@
 import firebase from "firebase/app";
+
 import "firebase/firestore";
+//console.log = console.warn = console.error = () => {};
+
+// Look ma, no error!
+//console.error('Something bad happened.');
 
 const firebaseConfig = {
     apiKey: "AIzaSyA1edmoDPFXQHGAUOCzX0CfSsJSnIZ7lLc",
@@ -10,6 +15,14 @@ const firebaseConfig = {
     appId: "1:797770083545:web:1ef45446422752b5cb62c8",
     measurementId: "G-6ZMJP299VC"
   };
-   firebase.initializeApp(firebaseConfig);
-
+   try {
+    firebase.initializeApp(firebaseConfig)
+  } catch (err) {
+    // we skip the “already exists” message which is
+    // not an actual error when we’re hot-reloading
+    if (!firebase.app.length) {
+      console.error('Firebase initialization error raised', err.stack)
+    }
+  }
+  
    export default firebase;
