b664f65f0d7f637cd25287e30f3cb0c547e25cd2
palindrome-kasak <64642473+palindrome-kasak@users.noreply.github.com>
2021-05-27 15:32:09 -0700

    firebase add to bed
    add firebase to bed

--- src/Pages/Beds.jsx
+++ src/Pages/Beds.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import firebase from 'firebase';
+import {useEffect , useState} from 'react'
 
-const Beds= () => {
+
+const db = firebase.database();
+const Beds= (props) => {
+const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    const dataref =db.ref(props.stateName+"Data")
+    dataref.once('value',async(snapshot)=>{
+      let data = await snapshot.val()
+       setdata(data)
+      console.log(data);
+    })
+  }, [])
+
+
   return (
     <>
-     
-<h1>Ambulance facility</h1>
-
-
+<h1>bed facility</h1>
+{data.filter(element => element.city == "Patna" ).map( (element,index) => <li key={index}>abx1.{element.city}</li> )}
     </>
   )
 };
