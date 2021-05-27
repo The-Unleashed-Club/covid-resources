56dbced17944a4c9892af2228d60091d7eff1b36
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2021-05-28 03:50:33 +0530

    UttarakhandData

---
+++ src/StatesLeads/Uttarakhand.jsx
@@ -0,0 +1,16 @@
+const UttarakhandData =[
+    {
+        City: "Roorkee",
+        nameOfResource: "oxygenCylinder",
+        Address: "Roorkee,Uttarakhand",
+        Contact: "9837060376",
+        Price: " ",
+        Comments: " ",
+        verficationDataTime: "4th May",
+        vendorName: "oxygen refill"
+    },
+ 
+];
+
+
+export default UttarakhandData
