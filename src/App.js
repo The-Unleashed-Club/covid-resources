be7e7fd568c20f81286d8a4ccec49b494b29c4c5
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2021-05-28 16:37:42 +0530

    Changes_in_routing

--- src/App.js
+++ src/App.js
@@ -8,17 +8,14 @@ import States from './components/states';
 import Nav from './components/NavBar';
 import About from '../src/NavPages/About';
 import Blog from '../src/NavPages/Blog';
-import Donate from '../src/NavPages/Donate';
-import Leads from '../src/NavPages/Leads';
+
 
 
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
 
 import Main from '../src/Pages/MainPage';
 
-//import Screen2 from './screens/Screen2'
 
-//context api
 import AppState from './context/AppState'
 
 
@@ -37,8 +34,7 @@ const App = () => {
 
               <Route path="/About" component={About} />
               <Route path="/Blog" component={Blog} />
-              <Route path="/Donate" component={Donate} />
-              <Route path="/Leads" component={Leads} />
+            
             </Switch>
 
           </AppState>
