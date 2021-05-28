941091a714a2704e050c0afb2059cb6f897e064b
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2021-05-28 12:21:22 +0530

    UpdatedRajasthanData

--- src/StatesLeads/Rajasthan.jsx
+++ src/StatesLeads/Rajasthan.jsx
@@ -1,27 +1,25 @@
-const RajasthanData =[
+const RajasthanData = [
     {
-        city:"Jaipur",
-        nameOfFacility:"Oxygen",
-        address: "Jaipur",
-        contact: "9352668996",
-        avalability: "",
-        price:"",
-        comments:"",
-        VerficationDataTime :"1:58 PM, 29-04-2021",
+        City: "Jaipur",
+        nameOfResource: "Oxygen",
+        Address: "Jaipur",
+        Contact: "9352668996",
+        Price: "",
+        Comments: "",
+        verficationDataTime: "1:58 PM, 29-04-2021",
         vendorName: " Ankit oxygen",
     },
     {
-        city:"Jaipur",
-        nameOfFacility:"oxygenCylinder",
-        address: "jaipur",
-        contact: "8272989242",
-        avalability: "",
-        price:"",
-        comments:"only avaibale if if anyonr wants 20 ltr or above",
-        VerficationDataTime :"8th may",
+        City: "Jaipur",
+        nameOfResource: "oxygenCylinder",
+        Address: "jaipur",
+        Contact: "8272989242",
+        Price: "",
+        Comments: "only avaibale if if anyonr wants 20 ltr or above",
+        verficationDataTime: "8th may",
         vendorName: "Oxygen Cylinder Jaipur ",
     },
-  
+
 
 ];
 
