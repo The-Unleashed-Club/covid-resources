c9477cfeadc64b8cba019902464c77ef15106ac3
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2021-06-18 02:05:57 +0530

    haryana

--- src/StatesLeads/Haryana.jsx
+++ src/StatesLeads/Haryana.jsx
@@ -9,7 +9,150 @@ const HaryanaData =[
         verficationDataTime: "",
         vendorName: "Gupta Oxygen, plot 152 sector 27",
     },
-
+    {
+        
+        City: "",
+        nameOfResource: "Ambulance",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "Beds",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "bloodBanks",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "foodDistribution",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "homeCare",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "Medicines",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "resourceAgencies",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "womenInCardiology",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "humanSupporters",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "Oxygen",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "oxygenCylinder",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+   
+    {
+    
+        City: "",
+        nameOfResource: "plasmaDonors",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
+    {
+    
+        City: "",
+        nameOfResource: "testingCenters",
+        Address: "",
+        Contact: "",
+        Price: " ",
+        Comments: "",
+        verficationDataTime: "",
+        vendorName: "",
+    },
 ];
 
 
